abf559c8f5552388fe5b8fd27575863d8e23e81c
Florian Leeber <flori@bin.org.in>
2020-12-14 21:20:50 +0100

    Move stuff to common device tree

--- common.mk
+++ common.mk
@@ -492,3 +492,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     libhfd_api
 
+# Ubuntu Touch
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ubuntu/network/main.conf:system/halium/etc/ofono/main.conf \
+    $(LOCAL_PATH)/ubuntu/network/ril_subscription.conf:system/halium/etc/ofono/ril_subscription.conf \
+    $(LOCAL_PATH)/ubuntu/bluetooth/bluebinder.conf:system/halium/etc/init/bluebinder.conf \
+    $(LOCAL_PATH)/ubuntu/70-dumpling.rules:system/halium/lib/udev/rules.d/70-android.rules \
+    $(LOCAL_PATH)/ubuntu/display.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
+    $(LOCAL_PATH)/ubuntu/config-default.xml:system/halium/usr/share/powerd/device_configs/config-default.xml
+
